de869d5666597c76722989e684dbec5e9340e100
rom4uk <macbook@Macbooks-MacBook-Pro.local>
2019-09-25 16:11:18 +0300

    Updates

---
+++ precache-manifest.801a3c7d7aac2af23916aa13bde5a1b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6061613cb5b1b331bc8b",
+    "url": "/pet/static/js/runtime~main.6061613c.js"
+  },
+  {
+    "revision": "9135e6ad06dedbb70ad1",
+    "url": "/pet/static/js/main.9135e6ad.chunk.js"
+  },
+  {
+    "revision": "1e24bb3081f37174bdae",
+    "url": "/pet/static/js/1.1e24bb30.chunk.js"
+  },
+  {
+    "revision": "9135e6ad06dedbb70ad1",
+    "url": "/pet/static/css/main.c0db6cc2.chunk.css"
+  },
+  {
+    "revision": "0039e7810e5373545d0a33657195a30c",
+    "url": "/pet/index.html"
+  }
+];
